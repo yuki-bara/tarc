b6d8847d2299b5dbc1ca55be19e766edc15938b3
yuki-bara <yukibara30@gmail.com>
2026-04-04 22:20:04 +0700

    fix: fix mod in tests and CI

--- internal/tests/go.mod
+++ internal/tests/go.mod
@@ -2,4 +2,4 @@ module test
 
 go 1.26.1
 
-require github.com/yuki-bara/tarc v0.0.0-20260404145428-82e21bf9c027 // indirect
+require github.com/yuki-bara/tarc v0.0.0-20260404151410-a429f46ece63 // indirect
--- internal/tests/main.go
+++ internal/tests/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/yuki-bara/tarc"
 )
 
 func main() {
+	fmt.Print("RUN TEST")
 	os.Mkdir("out", 0755)
 	err := tarc.Compressfile("test", "files/test.tar", "*")
 	if err != nil {
